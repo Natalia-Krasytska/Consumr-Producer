ccf3b315769753759f43afd07bdc1072be7e896e
natal <natalie77335@gmail.com>
2025-04-22 16:03:31 +0200

    First Commit

--- src/ait/mediation/BlkQueueImpl.java
+++ src/ait/mediation/BlkQueueImpl.java
@@ -1,20 +1,40 @@
 package ait.mediation;
 
-public class BlkQueueImpl<T> implements BlkQueue<T> {
+
+import java.util.LinkedList;
+
+public class BlkQueueImpl<X> implements BlkQueue<X> {
+    private final LinkedList<X> queue = new LinkedList<>();
+    private final int maxSize;
+
     public BlkQueueImpl(int maxSize) {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
+        this.maxSize = maxSize;
     }
 
     @Override
-    public void push(T message) {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
+    public synchronized void push(X message) {
+        while (queue.size() >= maxSize) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        queue.addLast(message);
+        notifyAll();
     }
 
     @Override
-    public T pop() {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
+    public synchronized X pop() {
+        while (queue.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        X message = queue.removeFirst();
+        notifyAll();
+        return message;
     }
 }
