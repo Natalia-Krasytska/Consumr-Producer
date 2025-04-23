af9ab77d1a77dcbc3a2c889e162ccc97b10854e0
natal <natalie77335@gmail.com>
2025-04-23 20:13:50 +0200

    Lock

--- src/ait/mediation/BlkQueueImpl.java
+++ src/ait/mediation/BlkQueueImpl.java
@@ -1,40 +1,55 @@
 package ait.mediation;
 
-
 import java.util.LinkedList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class BlkQueueImpl<T> implements BlkQueue<T> {
     private final LinkedList<T> queue = new LinkedList<>();
     private final int maxSize;
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Condition notFull = lock.newCondition();
+    private final Condition notEmpty = lock.newCondition();
 
     public BlkQueueImpl(int maxSize) {
         this.maxSize = maxSize;
     }
 
     @Override
-    public synchronized void push(T message) {
-        while (queue.size() >= maxSize) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+    public void push(T message) {
+        lock.lock();
+        try {
+            while (queue.size() >= maxSize) {
+                try {
+                    notFull.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
+
+            queue.addLast(message);
+            notEmpty.signalAll();
+        } finally {
+            lock.unlock();
         }
-        queue.addLast(message);
-        notifyAll();
     }
 
     @Override
-    public synchronized T pop() {
-        while (queue.isEmpty()) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+    public T pop() {
+        lock.lock();
+        try {
+            while (queue.isEmpty()) {
+                try {
+                    notEmpty.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
+            T message = queue.poll();
+            notFull.signalAll();
+            return message;
+        } finally {
+            lock.unlock();
         }
-        T message = queue.removeFirst();
-        notifyAll();
-        return message;
     }
 }
